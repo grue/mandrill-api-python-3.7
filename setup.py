b13cad86e158a13d1f12e9b11f355a7094c4aa0c
Joshua Sorenson <grue@users.noreply.github.com>
2020-01-19 16:42:13 -0800

    i am so disappointed in you mandrill.
    now to find a new email provider.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Mandrill Devs. Forked by Florian Mounier',
     author_email='community@mandrill.com',
     description='A CLI client and Python API library for the Mandrill email as a service platform. Forked fork python 3.7 support',
-    long_description=open(os.path.join(os.path.dirname(__file__), 'README')).read(),
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
     license='Apache-2.0',
     keywords='mandrill email api',
     url='https://bitbucket.org/mailchimp/mandrill-api-python/',
